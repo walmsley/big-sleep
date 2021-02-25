376d32a5da521e265dc34f0528c5bbb714836ec1
Will Walmsley <will@probablystudio.com>
2021-02-24 22:35:29 -0500

    grayscale mean loss abs inside squared

--- big_sleep/big_sleep.py
+++ big_sleep/big_sleep.py
@@ -180,15 +180,17 @@ class BigSleep(nn.Module):
         lat_loss = self.loss_coefs[1] * torch.abs(1024. - torch.sum(latents))
         lat_loss_2 = self.loss_coefs[2] * torch.mean(torch.abs(latents_reshaped.sum(dim=0) - ones_32))
         lat_loss_3 = self.loss_coefs[3] * torch.sum(torch.abs(latents_reshaped.max(dim=0)[0] - ones_32))
-        lat_loss_4 = self.loss_coefs[4] * torch.mean(torch.abs(img_grayscale-0.5))#torch.sum(torch.gt(img_grayscale,ones_256-0.1))
+        lat_loss_4 = self.loss_coefs[4] * torch.mean(torch.abs(img_grayscale-0.5)**2)#torch.sum(torch.gt(img_grayscale,ones_256-0.1))
 
-        print('losses', lat_loss.item(), lat_loss_2.item(), lat_loss_3.item(), lat_loss_4.item())
 
         # lat_loss =  torch.abs(1 - torch.std(latents, dim=1)).mean() + \
         #             torch.abs(torch.mean(latents, dim = 1)).mean() + \
         #             4 * torch.max(torch.square(latents).mean(), latent_thres)
 
         sim_loss = -self.loss_coefs[0] * torch.cosine_similarity(text_embed, image_embed, dim = -1).mean()
+
+        print('losses', lat_loss.item(), lat_loss_2.item(), lat_loss_3.item(), lat_loss_4.item(), sim_loss.item())
+
         return (lat_loss+lat_loss_2+lat_loss_3+lat_loss_4, sim_loss)
 
 class Imagine(nn.Module):
